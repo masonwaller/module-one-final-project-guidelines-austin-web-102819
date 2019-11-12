2b5bb46235c041a66bd9f610f51cca84e9c81d87
github username <github email address>
2019-11-12 15:41:42 -0600

    logging in info

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,64 @@
 require_relative '../config/environment'
 require 'pry'
 
+def welcome
+puts "Welcome top the NBA Quiz!!!!"
+puts "===================================================================================================="
+end
+def get_input
+    puts "1. Login"
+    puts "2. Create Account"
+    puts "3. Delete Account"
+    puts "Please enter 1, 2, or 3."
+    input = gets.chomp.to_i
+    if input == 1
+        login
+    elsif input == 2
+        create_account
+    elsif input == 3
+        delete_account
+    else 
+        get_input
+    end
+end
+def login
+    puts "Please enter your name."
+    name = gets.chomp.to_s
+    if User.find_by(name: name)
+        current = User.find_by(name: name)
+        $logged_in = current.id
+    else
+        puts "That user does not exist"
+        create_account
+    end
+end
+def create_account
+    puts "Please enter your name."
+    name = gets.chomp.to_s
+    User.create(name: name)
+    puts "Thanks for creating an account."
+    current = User.find_by(name: name)
+    $logged_in = current.id
+end
+def delete_account
+    puts "Enter your account name."
+    name = gets.chomp.to_s
+    if User.find_by(name: name)
+        User.find_by(name: name).destroy
+        puts "Your account has been deleted. We hope to see you again!"
+        get_input
+    else
+        puts "That user does not exist."
+        get_input
+    end
+end
 
-puts "HELLO WORLD"
-binding.pry
-0
+
+
+def run
+    welcome
+    number = get_input
+    
+end
+
+run
